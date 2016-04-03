c77b6212d20f5e4c6319e650973c0a13ca622487
Pavithra Purushoth <pavipretty19@gmail.com>
2016-04-03 18:33:28 +0530

    Update midElementSTACK.c

--- midElementSTACK.c
+++ midElementSTACK.c
@@ -6,7 +6,7 @@
 /*
 struct node
 {
-    int data;               // if the test case is given we can access the stack using structure
+    int data;               // if the test case is given we can access the stack using list
     struct node *link;
 }*top = NULL;
 */
@@ -14,7 +14,7 @@ int main(void)
 {
 	int stack[100]={10,12,1233,145,67,667,435,76,32,45,423};  //hard coded the stack since the elements are not known by the user
 	int top=0,i;
-	for(i=1;stack[i]!='\0';i++)
+	for(i=1;stack[i]!='\0';i++)	
 	{
 		top++;			// finding the total elements in a stack
 	}
